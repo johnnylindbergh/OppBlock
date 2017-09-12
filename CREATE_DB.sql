85d403834424f12b38d2bffb422aad4674e7c5fe
johnnylindbergh <johnnyl8@embarqmail.com>
2017-09-12 19:04:18 -0400

    finished recurring OppBlock Creation function

--- CREATE_DB.sql
+++ CREATE_DB.sql
@@ -1,67 +1,75 @@
 ALTER USER 'root'@'localhost' IDENTIFIED BY 'root';
+
 DROP DATABASE IF EXISTS opp_block;
-CREATE DATABASE IF NOT EXISTS opp_block;
+CREATE DATABASE opp_block;
 
 USE opp_block;
 
-CREATE TABLE IF NOT EXISTS opp_block_day (
+CREATE TABLE  opp_block_day (
     uid_day INT NOT NULL AUTO_INCREMENT,
-    day DATE ,
+    day DATETIME,
     PRIMARY KEY (uid_day)
 );
 
-CREATE TABLE IF NOT EXISTS groups (
+CREATE TABLE groups (
     uid_group INT NOT NULL AUTO_INCREMENT,
     group_info TEXT,
     PRIMARY KEY (uid_group)
 );
 
-CREATE TABLE IF NOT EXISTS excluded_groups (
+CREATE TABLE excluded_groups (
     uid_day INT,
     uid_group INT,
     FOREIGN KEY (uid_day) REFERENCES opp_block_day(uid_day),
     FOREIGN KEY (uid_group) REFERENCES groups(uid_group)
 );
 
-CREATE TABLE IF NOT EXISTS teachers (
+CREATE TABLE teachers (
     uid_teacher INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(32),
     teacher_info VARCHAR(32),
  	PRIMARY KEY (uid_teacher)
 );
 
-CREATE TABLE IF NOT EXISTS students (
+CREATE TABLE students (
     uid_student INT NOT NULL AUTO_INCREMENT,
-    student_info VARCHAR(32),
+    name VARCHAR(32),
     arrived TINYINT(1),
     PRIMARY KEY (uid_student)
-
 );
 
-CREATE TABLE IF NOT EXISTS student_groups (
+CREATE TABLE absent (
+    uid_student INT,
+    FOREIGN KEY (uid_student) REFERENCES students(uid_student)
+);
+
+CREATE TABLE student_groups (
     uid_student INT,
     uid_group INT,
     FOREIGN KEY (uid_student) REFERENCES students(uid_student),
     FOREIGN KEY (uid_group) REFERENCES groups(uid_group)
 );
 
-CREATE TABLE IF NOT EXISTS offerings (
+CREATE TABLE offerings (
     uid_offering INT NOT NULL AUTO_INCREMENT,
     name VARCHAR(32),
     max_size INT,
+    location VARCHAR(32),
+    materials VARCHAR(32),
     uid_teacher INT,
     recurring TINYINT(1),
     PRIMARY KEY (uid_offering),
     FOREIGN KEY (uid_teacher) REFERENCES teachers(uid_teacher)
 );
 
-CREATE TABLE IF NOT EXISTS calender (
+CREATE TABLE calender (
     uid_day INT,
     uid_offering INT,
     FOREIGN KEY (uid_day) REFERENCES opp_block_day(uid_day),
     FOREIGN KEY (uid_offering) REFERENCES opp_block_day(uid_day)
 );
 
-CREATE TABLE IF NOT EXISTS choices (
+CREATE TABLE choices (
     uid_day INT,
     uid_student INT,
     uid_offering INT DEFAULT NULL,
@@ -71,18 +79,21 @@ CREATE TABLE IF NOT EXISTS choices (
 
 );
 
-INSERT into opp_block_day (day) values ('1999-08-20');
-INSERT into opp_block_day (day) values ('2017-09-20');
+INSERT into opp_block_day (day) values ('1999-08-20 2:00:00.0');
+INSERT into opp_block_day (day) values ('2017-09-1 2:00:00.0');
+INSERT into opp_block_day (day) values ('2017-09-2 2:00:00.0');
+INSERT into opp_block_day (day) values ('2017-09-3 2:00:00.0');
 
-INSERT into teachers (teacher_info) values ('Mr. Minster');
+
+INSERT into teachers (name, teacher_info) values ('Mr. Minster', "he's a cool guy");
 
 INSERT into groups (group_info) values ('the lame group');
 INSERT into groups (group_info) values ('the cool group');
 
 INSERT into excluded_groups (uid_day, uid_group) values (1,1);
 
-INSERT into students (student_info) values ('Johnny');
-INSERT into students (student_info) values ('Derp');
+INSERT into students (name) values ('Johnny');
+INSERT into students (name) values ('Derp');
 
 INSERT into student_groups (uid_student, uid_group) values (1,2);
 INSERT into student_groups (uid_student, uid_group) values (2,1);
@@ -91,13 +102,13 @@ INSERT into offerings (name, max_size, uid_teacher, recurring) values ("The Mins
 INSERT into calender (uid_day, uid_offering) values (1,1);
 INSERT into calender (uid_day, uid_offering) values (2,1);
 
-INSERT into choices (uid_day, uid_student, uid_offering) values (1,1,NULL);
-INSERT into choices (uid_day, uid_student, uid_offering) values (1,2,NULL);
+INSERT into choices (uid_day, uid_student) values (1,1);
+INSERT into choices (uid_day, uid_student) values (1,2);
 
 UPDATE choices SET uid_offering = 1 WHERE uid_day = 1 AND uid_student = 1;
 
-INSERT into choices (uid_day, uid_student, uid_offering) values (2,1,NULL);
-INSERT into choices (uid_day, uid_student, uid_offering) values (2,2,NULL);
+INSERT into choices (uid_day, uid_student ) values (2,1);
+INSERT into choices (uid_day, uid_student ) values (2,2 );
 
 UPDATE choices SET uid_offering = 1 WHERE uid_day = 2 AND uid_student = 1;
 UPDATE choices SET uid_offering = 1 WHERE uid_day = 2 AND uid_student = 2;
--- server.js
+++ server.js
@@ -13,22 +13,43 @@ var con = mysql.createConnection({
 
 con.connect();
 
-con.query('SELECT day from opp_block_day', function(err, rows, fields) {
- if (!err){
-    
-    console.log('\nOppBlock days:')
-    for (var i in rows) {
-      var day = rows[i]["day"];
-      console.log('\t'+moment(day).format('dddd MMMM Do, YYYY [at] h:mm'));
+function test () {
+  con.query('SELECT day from opp_block_day', function(err, rows, fields) {
+   if (!err){
+      
+      console.log('\nOppBlock days:')
+      for (var i in rows) {
+        var day = rows[i]["day"];
+        console.log('\t'+moment(day).format('dddd MMMM Do, YYYY [at] h:mm'));
+      }
+
     }
+    else{
+      console.log('Error, are you sure you ran CREATE_DB.sql?');
+    }
+  });
+}
 
-  }
-  else{
-    console.log('Error, are you sure you ran CREATE_DB.sql?');
-  }
-});
 
-con.end();
+
+
+function createOffering ( name,  maxSize,  location,  materials,  recurring,  teacherName, uidTeacher) {
+  if (uidTeacher == null){
+  con.query('SELECT uid_teacher FROM teachers WHERE name=?', [teacherName], function(err, results) {
+    results = results[0];
+    var uidTeacher = results.uid_teacher;
+      console.log(uidTeacher);
+      con.query('INSERT into offerings (name, max_size, location, materials, uid_teacher, recurring) values (?,?,?,?,?,?);', [name, maxSize,location, materials, uidTeacher, recurring]); 
+  });
+  }else{
+     con.query('INSERT into offerings (name, max_size, location, materials, uid_teacher, recurring) values (?,?,?,?,?,?);', [name, maxSize,location, materials, uidTeacher, recurring]); 
+  }
+    if (recurring){
+      //do this
+    }
+}
+
+createOffering("newOppBlock",2,"The CS Lab", "nothing", true, "Mr. Minster");
 
 var server = app.listen(8080, function () {
   console.log('OppBlock server listening on port %s', server.address().port);
