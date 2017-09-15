45242747663c12c7f28dba98e8cc51645ba3ddef
Anecdotal <hduffy@students.stab.org>
2017-09-15 14:50:24 -0400

    wrote ma functions

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-
 var mysql = require('mysql');
 var moment = require('moment');
 var express = require('express');
@@ -33,9 +32,51 @@ con.end();
 var server = app.listen(8080, function () {
   console.log('OppBlock server listening on port %s', server.address().port);
 });
+//Function numStudents checks number of students in an offering and maybe gets their info
+//takes in an offering uid, a day uid, and a boolean getStudentInfo, telling it whether to just sum the students or whether to return their information as well
+//Returns
+function numStudents(uid_day, uid_offering, getStudentInfo, callback) {
+	var numStud = 0;
+	var studList = [];
+	con.query('SELECT * FROM choices').on('data', function(row) {
+		if(uid_offering == row.uid_offering && uid_day == row.uid_day) {
+			numStud += 1;
+			if(getStudentInfo) {
+				studList.push(row.uid_student);
+			};
+		};
+	}).on('end', function() {
+		if(getStudentInfo) {
+			infoList = [];		
+			con.query('SELECT * FROM students WHERE uid_student in (?)', [studList]).on('data', function(row) {
+				for(var i=0; i<studList.length; i++) {
+          if(row.uid_student == studList[i]) {
+    	  		infoList.push(row.student_info);
+        		};
+      		};
+			}).on('end', function() {
+				callback(numStud, infoList)
+			})
+		} else {
+			callback(numStud, null);
+		}
+	});
+}
+//Function takes in an offering, returns true or false whether its full or not
+function isOfferingFull(uid_day, uid_offering, callback) {
+  con.query('SELECT max_size FROM offerings WHERE uid_offering = ?', [uid_offering]).on('data', function(data) {
+    numStudents(uid_day, uid_offering, false, function(num, infoList){
+      if(num == data) {
+        callback(true);
+      } else {
+        callback(false);
+      }    
+    })
+  })
+}
 //Function gets Offerings and their teachers from database;
 //Returns list ('offerList') of Offering objects, with all necessary properties
-function getOfferings() {
+function getOfferings(callback) {
   function Offering(uid, name, description, maxSize, recurring, teacher) {
     this.uid = uid;
     this.name = name;
@@ -47,22 +88,23 @@ function getOfferings() {
   var offerList = [];
   con.query('SELECT * FROM offerings').on('data', 
     function(row){
-      var offerList.size() = new Offering(row.uid_offering, row.name, row.description, row.max_size, row.recurring, row.uid_teacher);
-      offerList.push(offerList.size());
+      var offerList.length = new Offering(row.uid_offering, row.name, row.description, row.max_size, row.recurring, row.uid_teacher);
+      offerList.push(offerList.length);
     }).on('end', function() {
       con.query('SELECT * FROM teachers').on('data',
         function(row) {
-          for(var i=0; i<offerList.size(); i++) {
+          for(var i=0; i<offerList.length; i++) {
             if(row.uid_teacher == offerList[i].teacher) {
               offerList[i].teacher = row.teacher_info;
             };
           };
         }).on('end', function() {
-          return offerList;
+          callback(offerList);
         })
     })
 };
 //takes in the student uid, offering uid and day uid
 function saveOffering(day, student, offering) {
-  con.query('INSERT INTO choices (uid_day, uid_student, uid_offering) VALUES ($1, $2, $3)', [day, student, offering]);
+  con.query('UPDATE choices (uid_day, uid_student, uid_offering) VALUES ($1, $2, $3)', [day, student, offering]);
 };
+//MAKE CALLBACK
