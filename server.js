7575b6d67cbeb76b68e43ddcb7ec967bff856935
malymilo <malymilo@gmail.com>
2017-09-18 22:46:24 -0400

    remove OppBlock

--- server.js
+++ server.js
@@ -1,17 +1,21 @@
 
+
 var mysql = require('mysql');
 var moment = require('moment');
 var express = require('express');
 var app = express();
 
 var con = mysql.createConnection({
-  host     : 'localhost',
-  user     : 'root',
-  password : 'root',
+  host     : '165.227.73.128',
+  user     : 'public',
+  password : 'JFC-xNc-U2x-YqN',
   database : 'opp_block'
 });
 
 con.connect();
+
+con.query('TRUNCATE DATABASE opp_block;');
+con.query('srouce CREATE_DB.sql');
 
 con.query('SELECT day from opp_block_day', function(err, rows, fields) {
  if (!err){
@@ -33,3 +37,16 @@ con.end();
 var server = app.listen(8080, function () {
   console.log('OppBlock server listening on port %s', server.address().port);
 });
+
+function removeOppblock(offeringid, dayid){
+	if (dayid!=null){
+	con.query('DELETE * FROM calender WHERE uid_offering=offeringid AND uid_day=dayid');
+	};
+	if(dayid==null){
+		con.query ('DELETE * FROM offerings WHERE uid_offering=offeringid');
+	};
+};
+
+//function makeTeacher(name, info){
+	//William got assigned this as well an his works so we're using his
+//};
