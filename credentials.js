69e26fc7dbc84ef7bf2ca34d3b67349d2a17d701
Johnny Lindbergh <johnnyl8@embarqmail.com>
2017-09-28 20:24:01 -0400

    Update credentials.js

--- credentials.js
+++ credentials.js
@@ -1,8 +1,8 @@
 var Creds = {
-    'accountSid':'AC390563e2e73718ab901a3f42d0ee7cae',
-    'authToken':'63f3451da39445dbf8ed9d8c36a472a6',
-    'CLIENT_ID':'827038446996-lrrntro5hskmu9aj1jc55nrmv9090jr0.apps.googleusercontent.com',
-    'MySQL_username':'root',
-    'MySQL_password':'root',
+    'accountSid':'',
+    'authToken':'',
+    'CLIENT_ID':'',
+    'MySQL_username':'',
+    'MySQL_password':'',
 }
 module.exports = Creds;
