cfb61e8a1253f3a7b3f1d8e5211863e4cceb6eec
cmackethan <cmackethan@students.stab.org>
2017-10-16 11:09:07 -0400

    CSV file added

--- server.js
+++ server.js
@@ -2,8 +2,15 @@ var mysql = require('mysql');
 var moment = require('moment');
 var express = require('express');
 var app = express();
-var parse = require('csv-parse');
-require('should');
+var parse = require('csv-parse'); //parse csv file, turn into javascript array
+require('should'); //test library
+var bb = require('express-busboy'); //take in file from user (in temp folder)
+
+bb.extend(app, {
+  upload: true,
+  path: '/path/to/save/files', //need to designate a path in OppBlock Folder -- possibly use html to upload file in website
+  allowedPath: /./
+});
 
 var con = mysql.createConnection({
   host     : 'localhost',
@@ -14,9 +21,26 @@ var con = mysql.createConnection({
 
 con.connect();
 
+//add CSV file of students to database
 function createStudentCSV(studentList) {
-  var input = //input
-
+  //use input if exists
+  if req.files != null {
+    req.files[0] = input;
+  }
+  else {
+    println("File was not read properly");
+    return;
+  }
+  //parse CSV using callback API
+  parse(input, function(err, output){
+    //use output.should.eql() to test parser
+  });
+  //add values in array to database
+  for (var i = 0; i < output.length; i + 7) {
+    conn.query('INSERT INTO students(student_lastname, student_firstname, student_grade, student_sport, student_advisor, student_gender, student_email) VALUES (?, ?, ?, ?, ?, ?, ?, ?);', [output[i], output[i+1], output[i+2], output[i+3], output[i+4], output[i+5], output[i+6]], function(result) {
+      callback(result);
+    });
+  }
 }
 
 //create student if nothing exists in database, update student_info if something does
@@ -25,13 +49,13 @@ function createStudent(studentlastName, studentFirstName, studentGrade, studentS
     if(results[0] == undefined) {
       //if nothing is found in database, create new student
       con.query('INSERT INTO students(student_lastname, student_firstname, student_grade, student_sport, student_advisor, student_gender, student_email) VALUES (?, ?, ?, ?, ?, ?, ?, ?);', [studentLastName, studentFirstName, studentGrade, studentSport, studentAdvisor, studentGender, studentEmail], function(result) {
-      callback(result);
+        callback(result);
       });
     }
     else {
       //if something is found, update student info
       con.query('UPDATE students SET student_lastname = ? , student_firstname, student_grade, student_sport, student_advisor, student_gender, student_email) WHERE student (uid_student = ?);', [studentLastName, studentFirstName, studentGrade, studentSport, studentAdvisor, studentGender, studentEmail, results[0].uid_student], function(result) {
-      callback(result);
+        callback(result);
       });
     }
   });
@@ -43,11 +67,7 @@ createStudent("name", function(res){
 });
 */
 
-createStudent("Conrad Mackethan", function(result) {
-});
-
-createStudent("Quint McNeely", function(result) {
-});
+//example student
 
 var server = app.listen(8080, function () {
   console.log('OppBlock server listening on port %s', server.address().port);
