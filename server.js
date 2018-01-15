027bed35899a7247563f70a0cca5c83642af3eb8
johnnylindbergh <johnnyl8@embarqmail.com>
2018-01-15 13:34:52 -0500

    offering calendar feature

--- server.js
+++ server.js
@@ -15,10 +15,10 @@ app.engine('html', mustacheExpress());
 app.set('views', __dirname + '/views');
 
 con.init();	//initialize system settings
-
+var admin = require("./admin.js").init(app);
 var routes = require('./routes.js')(app);
 var student = require("./student.js");
-var admin = require("./admin.js").init(app);
+
 
 var server = app.listen(8080, function() {
     console.log('OppBlock server listening on port %s', server.address().port);
