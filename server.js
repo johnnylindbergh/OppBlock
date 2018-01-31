4dacf73cabd37e65b2a7785bf93230c315a4b8c2
Conrad MacKethan <cmackethan@students.stab.org>
2018-01-31 15:21:55 -0500

    fuck a static

--- server.js
+++ server.js
@@ -15,6 +15,9 @@ app.use(bodyParser.urlencoded({
 app.engine('html', mustacheExpress());
 app.set('views', __dirname + '/views');
 
+app.use(express.staic('views'))
+app.use(express.static('public'))
+
 var admin = require("./admin.js").init(app);
 var routes = require('./routes.js')(app);
 var student = require("./student.js").init(app);
@@ -23,4 +26,3 @@ var student = require("./student.js").init(app);
 var server = app.listen(8080, function() {
     console.log('OppBlock server listening on port %s', server.address().port);
 });
-
