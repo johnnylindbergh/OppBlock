bf1234a95a6574ad2fbbb172cf2c3fdf5c60ef90
johnnylindbergh <johnnyl8@embarqmail.com>
2018-02-22 13:22:31 -0500

    changed current offering query

--- routes.js
+++ routes.js
@@ -66,12 +66,12 @@ module.exports = function(app) {
 		var uid_teacher = req.user.local.uid_teacher;
 		var currentOffering;
 
-		con.query('select * from opp_block_day join calendar on calendar.uid_day = opp_block_day.uid_day join offerings on offerings.uid_teacher = ? order by opp_block_day.day desc',[uid_teacher], function(err, resultsDay){
+		con.query('select * from opp_block_day join calendar on calendar.uid_day = opp_block_day.uid_day join offerings on offerings.uid_teacher = ? order by opp_block_day.day asc',[uid_teacher], function(err, resultsDay){
 			if (!err){
 				for (var i = 0; i < resultsDay.length; i++){
 
 					currentOffering = resultsDay[0];
-					if (moment(resultsDay[i].day).isAfter()){
+					if (moment(resultsDay[i].day).isBefore()){
 
 						currentOffering = resultsDay[i];
 
