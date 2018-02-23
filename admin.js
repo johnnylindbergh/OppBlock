7368323c2e9936ca7c7e8568aef52fed49349c00
Zach Minster <zach.minster@gmail.com>
2018-02-23 09:46:46 -0500

    Revert "Update for Count mopblock"
    This reverts commit 6a9106079b3cf681117996a740a583b0d93dd67b.

--- admin.js
+++ admin.js
@@ -170,7 +170,7 @@ module.exports =  {
 			var uid_day = req.params.day;
 			var date = req.params.date;
 			//	Gets all the students who haven't signed up and their names
-			con.query('SELECT students.uid_student, COUNT(students.uid_student) AS count, students.advisor, students.grade, CONCAT(students.lastname, \', \',students.firstname) AS studentname FROM choices JOIN students ON choices.uid_student = students.uid_student WHERE uid_offering IS NULL AND uid_day = ? ORDER BY students.grade, students.lastname, students.firstname DESC;', [uid_day], function(err, resultsMopblock){
+			con.query('SELECT students.uid_student, students.advisor, students.grade, CONCAT(students.lastname, \', \',students.firstname) AS studentname FROM choices JOIN students ON choices.uid_student = students.uid_student WHERE uid_offering IS NULL AND uid_day = ? ORDER BY students.grade, students.lastname, students.firstname DESC;', [uid_day], function(err, resultsMopblock){
 				if(!err) {
 					//	Renders the page
 					res.render('Mopblock.html', {date:date, data:resultsMopblock});
